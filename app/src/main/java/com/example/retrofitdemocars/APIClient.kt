e1b99c6bb0e2920791524c0fd840533046160a88
TMDStudios <tmdstudioscontact@gmail.com>
2022-09-08 15:03:28 -0400

    Added logging interceptor

--- app/src/main/java/com/example/retrofitdemocars/APIClient.kt
+++ app/src/main/java/com/example/retrofitdemocars/APIClient.kt
@@ -1,5 +1,7 @@
 package com.example.retrofitdemocars
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,9 +9,18 @@ class APIClient {
     private var retrofit: Retrofit? = null
 
     fun getClient(): Retrofit?{
+
+        // New Code
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
         retrofit = Retrofit.Builder()
             .baseUrl("https://raw.githubusercontent.com/AlminPiricDojo/JSON_files/main/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient) // New Code
             .build()
         return retrofit
     }
